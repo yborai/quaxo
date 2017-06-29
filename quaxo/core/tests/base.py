cfbe8d1e2150604078b115eaf21cc24119cc0095
Yoseph Borai <yborai@logicworks.net>
2017-06-29 11:52:56 -0400

    base unittest module added

---
+++ quaxo/core/tests/base.py
@@ -0,0 +1,35 @@
+from cement.utils import test
+
+from ...__main__ import Stub as Quaxo
+
+
+class QuaxoRunner(Quaxo):
+    class Meta:
+        argv = []
+
+    @classmethod
+    def assert_quaxo_success(cls, obj, args):
+        with cls(argv=args) as app:
+            with obj.assertRaises(SystemExit) as cm:
+                app.run()
+            obj.eq(
+                cm.exception.code, 0, msg="Expected to return SystemExit: 0"
+            )
+
+
+class TestQuaxo(test.CementTestCase):
+    app_class = QuaxoRunner
+
+    def setUp(self):
+        with self.app_class() as app:
+            log = app.log
+
+    def tearDown(self):
+        """
+        Cement uses temp files in its default tests, and the references
+        will not exist if setUp is overridden and will break when
+        tearDown is called. A short-term fix is to override tearDown as
+        well. The long-term fix will be to use CementTestCase more like
+        the authors of Cement intended.
+        """
+        pass
--- quaxo/core/tests/private_subnets_test.py
+++ quaxo/core/tests/private_subnets_test.py
@@ -1,45 +1,18 @@
-import os
-
 from cement.utils import test
 
-from ...__main__ import Stub as Quaxo
 from ...cli.scanners.private_subnets import get_private_subnets
+from .base import QuaxoRunner
 
-class Quaxo_(Quaxo):
-    class Meta:
-        argv = []
-
-    @classmethod
-    def assert_quaxo_success(cls, obj, args):
-        with cls(argv=args) as app:
-            with obj.assertRaises(SystemExit) as cm:
-                app.run()
-            obj.eq(cm.exception.code, 0, msg="Expected to return SystemExit: 0")
 
 class TestQuaxo(test.CementTestCase):
-    app_class = Quaxo_
-
-    def setUp(self):
-        with self.app_class() as app:
-            log = app.log
-
-    def tearDown(self):
-        """
-        Cement uses temp files in its default tests, and the references
-        will not exist if setUp is overridden and will break when
-        tearDown is called. A short-term fix is to override tearDown as
-        well. The long-term fix will be to use CementTestCase more like
-        the authors of Cement intended.
-        """
-        pass
 
     def test_private_subnets_help(self):
-        Quaxo_.assert_quaxo_success(self, [
+        QuaxoRunner.assert_quaxo_success(self, [
             "private-subnets", "--help"
         ])
 
     def test_get_private_subnets(self):
         subnets = ['subnet-d72c36ed', 'subnet-b98cbc85']
-        vpcs =    ['vpc-88ac4bec', 'vpc-a31ca1c4']
+        vpcs = ['vpc-88ac4bec', 'vpc-a31ca1c4']
         public_vpcs = ['vpc-88ac4bec']
         assert get_private_subnets(subnets, vpcs, public_vpcs) == ["subnet-b98cbc85"]
